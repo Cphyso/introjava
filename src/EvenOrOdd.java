a0af2d11da77c1c524a36c5c213abdb0874f7e0c
Cphyso <57254988+Cphyso@users.noreply.github.com>
2019-11-28 15:48:19 +0200

    Update EvenOrOdd.java

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
     public static void main(String args[]){
-        Scanner kb = new Scanner(System.in);
+        Scanner keyBoard = new Scanner(System.in);
         System.out.print("Enter an integer : ");
 
-        System.out.println(kb.nextInt()%2==0? "even" : "odd");
+        System.out.println(keyBoard.nextInt()%2==0? "even" : "odd");
     }
 }
